3bf017ac89b231befeb5e0882138b93ee572c4de
juhair-cupcake <juhair98@pm.me>
2023-04-05 02:20:27 +0600

    added: texture

--- main.c
+++ main.c
@@ -34,8 +34,8 @@ typedef struct Brick {
 #define BRICKS_LINES 5
 #define BRICKS_PER_LINE 20
 #define BRICKS_POSITION_Y 50
-#define screenWidth 800
-#define screenHeight 450
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
 
 //globa Var Decleare
 int frameCounter = 0, gameResult = -1;
@@ -44,12 +44,12 @@ Player player = { 0 };
 Ball ball = { 0 };
 Brick bricks[BRICKS_LINES][BRICKS_PER_LINE] = { 0 };
 GameScreen screen = LOGO;
+Texture2D texLogo, texBall, texPaddle, texBrick;
 
 void main() {
-  initialize();
-  
-  InitWindow(screenWidth, screenHeight, "ARKANOID");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "ARKANOID");
   SetTargetFPS(60);
+  initialize();
   
   //gameLoop
   while(!WindowShouldClose()) {
@@ -65,11 +65,21 @@ void main() {
     EndDrawing();
   }
   
+  //Bye
+  UnloadTexture(texLogo);
+  UnloadTexture(texPaddle);
+  UnloadTexture(texBall);
+  UnloadTexture(texBrick);
   CloseWindow();
 }
 
 void initialize() {
-  player.position = (Vector2){ screenWidth/2, screenHeight*7/8 };
+  texLogo = LoadTexture("./resources/raylib_logo.png");
+  texBall = LoadTexture("./resources/ball.png");
+  texPaddle = LoadTexture("./resources/paddle.png");
+  texBrick = LoadTexture("./resources/brick.png");
+  
+  player.position = (Vector2){ SCREEN_WIDTH/2, SCREEN_HEIGHT*7/8 };
   player.speed = (Vector2){ 8.0f, 0.0f };
   player.size = (Vector2){ 100, 24 };
   player.lifes = PLAYER_LIFES;
@@ -81,7 +91,7 @@ void initialize() {
   
   for (int y=0; y<BRICKS_LINES; y++){
     for(int x=0; x<BRICKS_PER_LINE; x++){
-      bricks[y][x].size = (Vector2){ screenWidth/BRICKS_PER_LINE, 20};
+      bricks[y][x].size = (Vector2){ SCREEN_WIDTH/BRICKS_PER_LINE, 20};
       bricks[y][x].position = (Vector2){ x*bricks[y][x].size.x, y*bricks[y][x].size.y + BRICKS_POSITION_Y };
       bricks[y][x].bounds = (Rectangle){ bricks[y][x].position.x, bricks[y][x].position.y, bricks[y][x].size.x, bricks[y][x].size.y };
       bricks[y][x].active = true;
@@ -106,14 +116,14 @@ void update() {
       if(IsKeyDown(KEY_RIGHT)) player.position.x += player.speed.x;
 
       if(player.position.x <= 0) player.position.x = 0;
-      if(player.position.x + player.size.x >= screenWidth) player.position.x = screenWidth - player.size.x;
+      if(player.position.x + player.size.x >= SCREEN_WIDTH) player.position.x = SCREEN_WIDTH - player.size.x;
 
       player.bounds = (Rectangle){ player.position.x, player.position.y, player.size.x, player.size.y };
 
       if(ball.active){
         ball.position.x += ball.speed.x;
         ball.position.y += ball.speed.y;
-        if(((ball.position.x + ball.radius) >= screenWidth ) || ((ball.position.x - ball.radius) <= 0 )) ball.speed.x *= -1;
+        if(((ball.position.x + ball.radius) >= SCREEN_WIDTH ) || ((ball.position.x - ball.radius) <= 0 )) ball.speed.x *= -1;
         if((ball.position.y + ball.radius) <= 0 ) ball.speed.y *= -1;
 
         //ball VS player
@@ -134,7 +144,7 @@ void update() {
         }
         
         //Game Lose
-        if((ball.position.y + ball.radius) >= screenHeight){
+        if((ball.position.y + ball.radius) >= SCREEN_HEIGHT){
           ball.position.x = player.position.x + player.size.x/2;
           ball.position.y = player.position.y - ball.radius - 1.0f;
           ball.speed = (Vector2){ 0, 0 };
@@ -168,10 +178,11 @@ void draw() {
   ClearBackground(RAYWHITE);
   
   if (screen == LOGO) {
-    DrawText("LOGO", 20, 20, 80, LIGHTGRAY);
+    //DrawText("LOGO", 20, 20, 80, LIGHTGRAY);
+    DrawTexture(texLogo, SCREEN_WIDTH/2 - texLogo.width/2, SCREEN_HEIGHT/2 - texLogo.height/2, WHITE);
   }
   else if (screen == TITLE) {
-    DrawText("Yo Yo Chiki Chiki", 20, 20, 40, DARKGREEN);
+    DrawText("PRESS TO PLAY", 20, 20, 40, DARKGREEN);
     if ((frameCounter/30)%2 == 0) {
       char *text = "Press [ENTER] to Start";
       int fontSize = 20;
@@ -179,18 +190,22 @@ void draw() {
     }
   } 
   else if (screen == GAMEPLAY) {
-    DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, BLACK);
-    DrawCircleV(ball.position, ball.radius, MAROON);
+    //DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, BLACK);
+    DrawTextureEx(texPaddle, player.position, 0.0f, 1.0f, WHITE);
+    //DrawCircleV(ball.position, ball.radius, MAROON);
+    DrawTexture(texBall, ball.position.x - ball.radius/2, ball.position.y - ball.radius/2, MAROON);
 
     //draw brick
     for(int y=0; y<BRICKS_LINES; y++){
       for(int x=0; x<BRICKS_PER_LINE; x++){
         if(bricks[y][x].active) {
           if((x+y)%2 == 0) {
-            DrawRectangle(bricks[y][x].position.x, bricks[y][x].position.y, bricks[y][x].size.x, bricks[y][x].size.y, GRAY);
+            //DrawRectangle(bricks[y][x].position.x, bricks[y][x].position.y, bricks[y][x].size.x, bricks[y][x].size.y, GRAY);
+            DrawTextureEx(texBrick, bricks[y][x].position, 0.0f, 1.0f, GRAY);
           }
           else {
-            DrawRectangle(bricks[y][x].position.x, bricks[y][x].position.y, bricks[y][x].size.x, bricks[y][x].size.y, DARKGRAY);
+            //DrawRectangle(bricks[y][x].position.x, bricks[y][x].position.y, bricks[y][x].size.x, bricks[y][x].size.y, DARKGRAY);
+            DrawTextureEx(texBrick, bricks[y][x].position, 0.0f, 1.0f, DARKGRAY);
           }
         }
       }
@@ -198,13 +213,13 @@ void draw() {
 
     //GUI
     //playerLife
-    for(int i=0; i<player.lifes; i++) DrawRectangle(20 + 40*i, screenHeight-30, 35, 10, LIGHTGRAY);
+    for(int i=0; i<player.lifes; i++) DrawRectangle(20 + 40*i, SCREEN_HEIGHT-30, 35, 10, LIGHTGRAY);
 
     char *pauseText = "PAUSED";
-    if(gamePaused) DrawText(pauseText, screenWidth/2 - MeasureText(pauseText, 40)/2, screenHeight/2 + 60, 40, GRAY);
+    if(gamePaused) DrawText(pauseText, SCREEN_WIDTH/2 - MeasureText(pauseText, 40)/2, SCREEN_HEIGHT/2 + 60, 40, GRAY);
   }
   else if (screen == ENDING) {
-    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+    DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, BLUE);
     char *endText = "Press [ENTER] to Play Again";
     if((frameCounter/30)%2 == 0) DrawText(endText, GetScreenWidth()/2 - MeasureText(endText, 20)/2, GetScreenHeight()/2 +80, 20, GRAY);
   }
